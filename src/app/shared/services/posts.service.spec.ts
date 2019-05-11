01692c4b75b8884a4f82f6aaacc8317a72fc40ce
Aleksandr Sokolov <asok10@yandex.ru>
2019-05-11 18:30:33 +0200

    Trigger rebuild

---
+++ src/app/shared/services/posts.service.spec.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {BaseApi} from '../core/base-api';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+@Injectable()
+export class PostsService extends BaseApi {
+
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  getPosts(data): Observable<any> {
+    return this.get(`posts`, data);
+  }
+
+  getCommentToPost(postId): Observable<any> {
+    return this.get(`posts/${postId}/replies/`);
+  }
+
+}
